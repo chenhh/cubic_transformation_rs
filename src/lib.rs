b8d7054924cd18811aefbcf5d879cac0d24f556a
chenhh <chenhh@eva00>
2023-06-10 10:34:01 +0800

    update crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+pub struct Moment{
+    mean: f64,
+    var: f64,
+    skew: f64,
+    ex_kurt: f64
+}
+
+pub fn cubic_transformation(target_mom: &Moment, n_scenario: u32) -> Vec<f64>{
+    let scenarios = Vec::new();
+    scenarios
+}
+
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
