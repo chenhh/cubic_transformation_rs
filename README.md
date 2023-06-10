a64ed488a853cc13fa69835c7e2b7c226541c67d
chenhh <chenhh@t480s>
2023-06-10 22:43:01 +0800

    least square optimization

--- README.md
+++ README.md
@@ -6,9 +6,41 @@
 * A.I. Fleishman, “A method for simulating nonnormal distributions,” Psychometrika, vol. 43, pp. 521–532, 1978.
 * Kjetil Hoyland, Michal Kaut, and Stein W. Wallace, "A Heuristic for Moment-Matching Scenario Generation," Computational Optimization and Applications, Vol. 24, pp. 169-185, 2003.
 
-# 實作細節
+# crate
 
-1. 給定一個隨機變數Y的平均值(mean)、變異數(variance)、偏度(skewness)、超峰度(ex-kurtosis)，由公式(不偏或是漸近不偏)反推出前4階(原始)動差(moments)EY1, EY2, EY3, EY4 。
-2. 由標準常態分佈取出樣本n_scenario個隨機變數X，且算出X的前12階原始動差 EX1, EX2,..., EX12。
-3. cubic transform: Y=a + bX + cX**2 + dX**3, 求出a,b,c,d即可由X生成目標樣本Y。
+* [gomez](https://docs.rs/gomez/latest/gomez/index.html)
+* [levenberg_marquardt](https://docs.rs/levenberg-marquardt/latest/levenberg_marquardt/)
+
+# 演算法
+
+由標準常態分佈X~N(0,1) 抽出S個樣本。
+
+可由cubic transform Y=a+ bX + cX^2 + dX^3得到所要的隨機樣本Y，
+其中Y的mean, variance, skew, kurtosis為指定之值。因此問題在於如何得到參數a,b,c,d之值。
+
+可用nonlinear least-square算法求出a,b,c,d之值, 但必須給定X的前12階動差, error function, 
+error function的gradient vector與hessian matrix。
+
+注意a,b,c,d不一定有解，此時給出最小化錯誤平方之參數值即可。
+
+已知關係式 Y=a+ bX + cX^2 + dX^3
+兩側取期望值得 E(Y) = a + bE(X) + cE(x^2) + dE(X^3) = f(a,b,c,d)
+
+Y^2 = (a+bX+cX^2 + d X^3)^2  兩側取期望值得E(Y^2)
+Y^3 = (a+bX+cX^2 + d X^3)^3  兩側取期望值得E(Y^3)
+Y^4 = (a+bX+cX^2 + d X^3)^4  兩側取期望值得E(Y^4)
+
+由於已經給定目標前4階(中央)動差，即可反推出E(Y), E(Y^2), E(Y^3), E(Y^4)
+可得least square的目標函數為最小化g=
+    (E(Y)- f(a,b,c,d))^2 +
+    (E(Y^2)- f^2(a,b,c,d))^2 +
+    (E(Y^3)- f^3(a,b,c,d))^2 +
+    (E(Y^4)- f^4(a,b,c,d))^2的(a,b,c,d)之值。
+
+
+可得gradient vector為[
+
+
+]
+ 
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,59 +6,106 @@ pub struct Statistics {
     var: f64,
     skew: f64,
     ex_kurt: f64,
+    n_sample: u32,
 }
 
+// impl Statistics {
+//     pub fn moments(&self, bias: bool) -> Option<[f64; 4]> {
+//         let mut moments = [0f64; 4];
+//         if self.n_sample <= 2 {
+//             return None;
+//         }
+//         moments[0] = self.n_sample as f64 * self.mean;
+//
+//         if bias == true {
+//             // biased estimator
+//             moments[1] = self.n_sample as f64 * self.var;
+//         } else {
+//             // unbiased estimator
+//             moments[1] = (self.n_sample - 1) as f64 * self.var;
+//         }
+//
+//         Some(moments)
+//     }
+// }
+
 pub fn cubic_transformation(tgt_stats: &Statistics, n_scenario: usize) -> Vec<f64> {
-    let scenarios = Vec::with_capacity(n_scenario);
-    init_params = [0f64, 1f64, 0f64, 0f64];
-    let val: f64 = thread_rng().sample(StandardNormal);
+    let mut scenarios = Vec::with_capacity(n_scenario);
+
+    // generating standard normal distribution samples
+    let mut rng = thread_rng();
+    let samples: Vec<f64> = StandardNormal
+        .sample_iter(&mut rng)
+        .take(n_scenario)
+        .collect();
+
+    let mut ex = [0f64; 12];
+    for idx in 0..ex.len() {
+        ex[idx] = samples
+            .iter()
+            .map(|x| x.powf((idx + 1) as f64))
+            .sum::<f64>()
+            / tgt_stats.n_sample as f64;
+    }
+
+    // to generate samples Y with zero mean, unit variance, the same skew and kurt with tgt.
+    let mut y_moments = [0f64, 1f64, tgt_stats.skew, tgt_stats.ex_kurt + 3.];
+
+    // using least square error algorithm to get params
+    let mut params = [0f64, 1f64, 0f64, 0f64];
+
+    scenarios
+        .iter_mut()
+        .map(|x| params[0] + params[1] * x + params[2] * x.powf(2.) + params[3] * x.powf(3.));
 
     scenarios
 }
 
 fn cubic_errors(params: [f64; 4], EX: [f64; 12], EY: [f64; 4]) -> [f64; 4] {
-    let (a, b, c, d) = params;
+    let (a, b, c, d) = (params[0], params[1], params[2], params[3]);
     let (mut v1, mut v2, mut v3, mut v4) = (0f64, 0f64, 0f64, 0f64);
 
     v1 = a + b * EX[0] + c * EX[1] + d * EX[2] - EY[0];
 
     v2 = (d * d * EX[5]
-        + 2 * c * d * EX[4]
-        + (2 * b * d + c * c) * EX[3]
-        + (2 * a * d + 2 * b * c) * EX[2]
-        + (2 * a * c + b * b) * EX[1]
-        + 2 * a * b * EX[0]
+        + 2. * c * d * EX[4]
+        + (2. * b * d + c * c) * EX[3]
+        + (2. * a * d + 2. * b * c) * EX[2]
+        + (2. * a * c + b * b) * EX[1]
+        + 2. * a * b * EX[0]
         + a * a
         - EY[1]);
 
     v3 = ((d * d * d) * EX[8]
-        + (3 * c * d * d) * EX[7]
-        + (3 * b * d * d + 3 * c * c * d) * EX[6]
-        + (3 * a * d * d + 6 * b * c * d + c * c * c) * EX[5]
-        + (6 * a * c * d + 3 * b * b * d + 3 * b * c * c) * EX[4]
-        + (a * (6 * b * d + 3 * c * c) + 3 * b * b * c) * EX[3]
-        + (3 * a * a * d + 6 * a * b * c + b * b * b) * EX[2]
-        + (3 * a * a * c + 3 * a * b * b) * EX[1]
-        + 3 * a * a * b * EX[0]
+        + (3. * c * d * d) * EX[7]
+        + (3. * b * d * d + 3. * c * c * d) * EX[6]
+        + (3. * a * d * d + 6. * b * c * d + c * c * c) * EX[5]
+        + (6. * a * c * d + 3. * b * b * d + 3. * b * c * c) * EX[4]
+        + (a * (6. * b * d + 3. * c * c) + 3. * b * b * c) * EX[3]
+        + (3. * a * a * d + 6. * a * b * c + b * b * b) * EX[2]
+        + (3. * a * a * c + 3. * a * b * b) * EX[1]
+        + 3. * a * a * b * EX[0]
         + a * a * a
         - EY[2]);
 
     v4 = ((d * d * d * d) * EX[11]
-        + (4 * c * d * d * d) * EX[10]
-        + (4 * b * d * d * d + 6 * c * c * d * d) * EX[9]
-        + (4 * a * d * d * d + 12 * b * c * d * d + 4 * c * c * c * d) * EX[8]
-        + (12 * a * c * d * d + 6 * b * b * d * d + 12 * b * c * c * d + c * c * c * c) * EX[7]
-        + (a * (12 * b * d * d + 12 * c * c * d) + 12 * b * b * c * d + 4 * b * c * c * c) * EX[6]
-        + (6 * a * a * d * d
-            + a * (24 * b * c * d + 4 * c * c * c)
-            + 4 * b * b * b * d
-            + 6 * b * b * c * c)
+        + (4. * c * d * d * d) * EX[10]
+        + (4. * b * d * d * d + 6. * c * c * d * d) * EX[9]
+        + (4. * a * d * d * d + 12. * b * c * d * d + 4. * c * c * c * d) * EX[8]
+        + (12. * a * c * d * d + 6. * b * b * d * d + 12. * b * c * c * d + c * c * c * c) * EX[7]
+        + (a * (12. * b * d * d + 12. * c * c * d) + 12. * b * b * c * d + 4. * b * c * c * c)
+            * EX[6]
+        + (6. * a * a * d * d
+            + a * (24. * b * c * d + 4. * c * c * c)
+            + 4. * b * b * b * d
+            + 6. * b * b * c * c)
             * EX[5]
-        + (12 * a * a * c * d + a * (12 * b * b * d + 12 * b * c * c) + 4 * b * b * b * c) * EX[4]
-        + (a * a * (12 * b * d + 6 * c * c) + 12 * a * b * b * c + b * b * b * b) * EX[3]
-        + (4 * a * a * a * d + 12 * a * a * b * c + 4 * a * b * b * b) * EX[2]
-        + (4 * a * a * a * c + 6 * a * a * b * b) * EX[1]
-        + (4 * a * a * a * b) * EX[0]
+        + (12. * a * a * c * d + a * (12. * b * b * d + 12. * b * c * c) + 4. * b * b * b * c)
+            * EX[4]
+        + (a * a * (12. * b * d + 6. * c * c) + 12. * a * b * b * c + b * b * b * b) * EX[3]
+        + (4. * a * a * a * d + 12. * a * a * b * c + 4. * a * b * b * b) * EX[2]
+        + (4. * a * a * a * c + 6. * a * a * b * b) * EX[1]
+        + (4. * a * a * a * b) * EX[0]
         + a * a * a * a
         - EY[3]);
 
