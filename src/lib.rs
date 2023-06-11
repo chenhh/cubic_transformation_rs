e771b4d25c2caf928648b132492a9a8ce295e0fe
chenhh <chenhh@t480s>
2023-06-11 21:36:58 +0800

    least square optimization

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,43 @@ pub struct Statistics {
     n_sample: u32,
 }
 
+fn variance(samples: &Vec<f64>) -> f64 {
+    let n = samples.len();
+    let mu = samples.iter().sum::<f64>() / n as f64;
+    let var = samples.iter().map(|x| (*x - mu).powf(2.)).sum::<f64>() / (n - 1) as f64;
+    var
+}
+
+fn skewness(samples: &Vec<f64>) -> f64 {
+    /* biased estimator, as the same default value of scipy.stats.kurtosis */
+    let n = samples.len();
+    let mu = samples.iter().sum::<f64>() / n as f64;
+    let (mut m3, mut s3) = (0f64, 0f64);
+
+    for idx in 0..n {
+        m3 += (samples[idx] - mu).powf(3.);
+        s3 += (samples[idx] - mu).powf(2.);
+    }
+    m3 /= n as f64;
+    s3 /= n as f64;
+    m3 / s3.powf(1.5)
+}
+
+fn kurtosis(samples: &Vec<f64>) -> f64 {
+    /* biased estimator, as the same default value of scipy.stats.kurtosis */
+    let n = samples.len();
+    let mu = samples.iter().sum::<f64>() / n as f64;
+    let (mut m4, mut s4) = (0f64, 0f64);
+
+    for idx in 0..n {
+        m4 += (samples[idx] - mu).powf(4.);
+        s4 += (samples[idx] - mu).powf(2.);
+    }
+    m4 /= n as f64;
+    s4 /= n as f64;
+    m4 / s4.powf(2.) - 3.
+}
+
 pub fn cubic_transformation_sampling(tgt_stats: &Statistics, n_scenario: usize) -> Vec<f64> {
     // generating standard normal distribution samples
     let mut rng = thread_rng();
@@ -42,17 +79,17 @@ fn residuals(params: [f64; 4], EX: [f64; 12], EZ: [f64; 4]) -> [f64; 4] {
 
     r1 = a + b * EX[0] + c * EX[1] + d * EX[2] - EZ[0];
 
-    r2 = (d * d * EX[5]
+    r2 = d * d * EX[5]
         + 2. * c * d * EX[4]
         + (2. * b * d + c * c) * EX[3]
         + (2. * a * d + 2. * b * c) * EX[2]
         + (2. * a * c + b * b) * EX[1]
         + 2. * a * b * EX[0]
         + a * a
-        - EZ[1]);
+        - EZ[1];
 
-    r3 = ((d * d * d) * EX[8]
-        + (3. * c * d * d) * EX[7]
+    r3 = d * d * d * EX[8]
+        + 3. * c * d * d * EX[7]
         + (3. * b * d * d + 3. * c * c * d) * EX[6]
         + (3. * a * d * d + 6. * b * c * d + c * c * c) * EX[5]
         + (6. * a * c * d + 3. * b * b * d + 3. * b * c * c) * EX[4]
@@ -61,9 +98,9 @@ fn residuals(params: [f64; 4], EX: [f64; 12], EZ: [f64; 4]) -> [f64; 4] {
         + (3. * a * a * c + 3. * a * b * b) * EX[1]
         + 3. * a * a * b * EX[0]
         + a * a * a
-        - EZ[2]);
+        - EZ[2];
 
-    r4 = ((d * d * d * d) * EX[11]
+    r4 = (d * d * d * d) * EX[11]
         + (4. * c * d * d * d) * EX[10]
         + (4. * b * d * d * d + 6. * c * c * d * d) * EX[9]
         + (4. * a * d * d * d + 12. * b * c * d * d + 4. * c * c * c * d) * EX[8]
@@ -82,13 +119,9 @@ fn residuals(params: [f64; 4], EX: [f64; 12], EZ: [f64; 4]) -> [f64; 4] {
         + (4. * a * a * a * c + 6. * a * a * b * b) * EX[1]
         + (4. * a * a * a * b) * EX[0]
         + a * a * a * a
-        - EZ[3]);
+        - EZ[3];
 
     [r1, r2, r3, r4]
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
 }
 
 #[cfg(test)]
@@ -96,8 +129,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_variance() {
+        let samples: Vec<f64> = (0..10).map(|x| x as f64).collect();
+        let res = variance(&samples);
+        println!("variance:{res}");
+        assert_eq!(res, 8.25)
     }
 }
